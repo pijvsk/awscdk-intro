339e5abd652686e16e10e50509738ac82de9b940
pijvsk <pijus.vaskevicius@telesoftas.com>
2022-06-02 18:14:08 +0300

    fix: dont randomize bucket name

--- lib/awscdk-intro-stack.greeter-lambda.ts
+++ lib/awscdk-intro-stack.greeter-lambda.ts
@@ -1,21 +1,21 @@
 import { S3 } from "aws-sdk";
 
-
 const bucketName = process.env.bucket!;
 export const handler = async function (event: any, context: any) {
-    try {
-        const body = 'hello world';
-        const s3 = new S3();
-        await s3.upload({
-            Bucket: bucketName,
-            Key: `${new Date().getTime()}`,
-            Body: body
-        }).promise()
+  try {
+    const body = "hello world";
+    const s3 = new S3();
+    await s3
+      .upload({
+        Bucket: bucketName,
+        Key: `${new Date().getTime()}`,
+        Body: body,
+      })
+      .promise();
 
-      
-        return body;
-    } catch(e) {
-        console.log(e);
-        throw e;
-    }
-}
+    return body;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
--- lib/awscdk-intro-stack.ts
+++ lib/awscdk-intro-stack.ts
@@ -1,21 +1,21 @@
-import { Stack, StackProps, aws_s3, RemovalPolicy } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Stack, StackProps, aws_s3, RemovalPolicy } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class AwscdkIntroStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const greetingTrashcan = new aws_s3.Bucket(this, 'greeting-trashcan1234512312412', {
-      removalPolicy: RemovalPolicy.DESTROY
-    })
+    const greetingTrashcan = new aws_s3.Bucket(this, "greetings-mailbox", {
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
 
-    const greeterLambda = new lambda.NodejsFunction(this, 'greeter-lambda', {
+    const greeterLambda = new lambda.NodejsFunction(this, "greeter-lambda", {
       environment: {
-        bucket: greetingTrashcan.bucketName
-      }
-    })
+        bucket: greetingTrashcan.bucketName,
+      },
+    });
 
-    greetingTrashcan.grantWrite(greeterLambda)
+    greetingTrashcan.grantWrite(greeterLambda);
   }
 }
